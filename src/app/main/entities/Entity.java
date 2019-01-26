ca2d06694a5cef9b86506463d4bacc93fb3c5659
WillTarte <William.tarte6@gmail.com>
2019-01-26 12:26:09 -0500

    Added to entity class

--- src/app/main/entities/Entity.java
+++ src/app/main/entities/Entity.java
@@ -1,5 +1,7 @@
 package app.main.entities;
 
+import java.awt.Shape;
+
 public class Entity {
 	
 	private double x, y;
@@ -7,5 +9,15 @@ public class Entity {
 	private String id;
 	
 	private Vector velocity;
-
+	
+	private Shape collision_rectangle;
+	
+	
+	public Entity(double x, double y, String id, Vector velocity, Shape collision_rectangle) {
+		this.x = x;
+		this.y = y;
+		this.id = id;
+		this.velocity = velocity;
+		this.collision_rectangle = collision_rectangle;
+	}
 }
